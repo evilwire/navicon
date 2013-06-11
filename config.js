f867a6c1ed7c5167d11a0b7e3b1c2ad49d406230
drupal <drupal@localhost.localdomain>
2013-06-11 06:15:07 -0400

    added a require js config file

---
+++ config.js
@@ -0,0 +1,37 @@
+requirejs.config({
+   baseUrl : 'lib',
+   
+   paths : {
+      'eve'     : 'raphael/eve/eve',
+      'raphael' : 'raphael/raphael'
+   },
+
+   shim : {
+      'backbone' : {
+         deps : ['underscore', 'jquery'],
+         exports : 'Backbone'
+      },
+
+      'underscore' : {
+         exports : '_'
+      },
+
+      'handlebars' : {
+         exports : 'HB'
+      },
+
+      'raphael' : {
+         deps : ['eve'],
+         exports : 'Raphael'
+      }
+   }
+});
+
+requirejs(['jquery', 'backbone', 'underscore', 'raphael'],
+function( $, Backbone, _, Raphael )
+{
+   // setup code here
+   console.log( Raphael );
+   var paper = Raphael(10, 50, 320, 200);
+   //paper.path("M10,10C0,100,20,40,150,150");
+});
